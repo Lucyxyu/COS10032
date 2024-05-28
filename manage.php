6875bc9297bd4e76ba6295b76b7c7cc9b3f718d3
Lucyxyu <163368562+Lucyxyu@users.noreply.github.com>
2024-05-29 00:00:29 +1000

    added supervisor page template

---
+++ manage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="supervisor page">
+    <meta name="keywords" content="javascript, educational, mysqli, php, supervisor">
+    <meta name="author" content="Eloise Ridder-Strickland, Oshadi Dewmini Pattiarachchi, Lucy Yu">
+    <link rel="icon" type="image/x-icon" href="images/icon/favicon.ico">
+    <title>Supervisor page</title>
+    <!-- embedded code for external google font "Share Tech" -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Share+Tech&display=swap" rel="stylesheet">
+    <!-- css  -->
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+    <!-- HEADER -->
+    <?php
+        include_once("header.inc");
+    ?>
+
+    <!-- PAGE CONTENT -->
+    <article>
+        <h1 class="page-title">Quiz supervisor page</h1>
+        <p>blah blah</p>
+
+    <!-- FOOTER -->
+    <?php
+        include_once("footer.inc");
+    ?>
+    
+</body>
+
+
+
+</html>
